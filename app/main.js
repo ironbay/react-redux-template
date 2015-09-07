afaad7017121a32d85d279eb28cb426d3e48f767
Dax Ravi <d@ironbay.digital>
2015-09-07 05:29:59 -0400

    Version bump plus assorted goodies

--- app/main.js
+++ app/main.js
@@ -2,16 +2,11 @@ import React from 'react';
 import Router from 'react-router';
 import BrowserHistory from 'react-router/lib/BrowserHistory';
 import routes from './routes';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
-import * as reducers from "reducers"
 import { Provider } from 'react-redux';
 import Root from "root"
+import store from 'store'
 
 const history = new BrowserHistory();
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-const reducer = combineReducers(reducers);
-const store = createStoreWithMiddleware(reducer);
 const element = (
   <Provider store={store}>
     {() => <Router history={history} routes={routes} /> }
