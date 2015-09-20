c01ef4ed7564dd51de67f4ed4122eb91a101cb85
Dax <d@ironbay.digital>
2015-09-20 19:22:13 -0400

    Update main.js

--- app/main.js
+++ app/main.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import createHistory from 'history/lib/createBrowserHistory'
 import Root from "root"
 import store from 'store'
-console.log(createHistory)
 const element = (
   <Provider store={store}>
     {() => <Router history={createHistory()} routes={routes} /> }
