b7765170dba51220e49d30e149b3dd2ab3c7242d
Dax Ravi <d@ironbay.digital>
2015-09-29 02:44:50 -0400

    Linting

--- app/main.js
+++ app/main.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import Router from 'react-router';
-import routes from './routes';
-import { Provider } from 'react-redux';
+import React from 'react'
+import Router from 'react-router'
+import routes from './routes'
+import { Provider } from 'react-redux'
 import createHistory from 'history/lib/createBrowserHistory'
-import Root from "root"
 import store from 'store'
 const element = (
   <Provider store={store}>
     {() => <Router history={createHistory()} routes={routes} /> }
   </Provider>
-);
+)
 React.render(element, document.body);
