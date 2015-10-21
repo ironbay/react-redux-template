e956196188224effc53c19295790d428d2e4ccad
Dax Ravi <d@ironbay.digital>
2015-10-21 16:28:59 -0400

    Sync

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,16 +1,21 @@
-var path = require('path')
-var webpack = require('webpack');
-var config = require("./prod.config.js");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+var webpack = require('webpack')
+var config = require('./prod.config.js')
 
-config.devtool = "cheap-module-eval-source-map";
+config.devtool = 'cheap-module-eval-source-map'
 
-config.entry.push('webpack/hot/only-dev-server');
+config.entry.push('webpack/hot/only-dev-server')
+
+config.module.loaders[0].loader = 'react-hot!' + config.module.loaders[0].loader
+config.module.loaders[2].loader = 'style-loader!css-loader!cssnext-loader'
+
 config.plugins[1] = new webpack.HotModuleReplacementPlugin()
-config.module.loaders[0].loader = "react-hot!babel?stage=0"
-config.module.loaders[2].loader = "style-loader!css-loader?sourceMap!cssnext-loader"
+config.plugins[2] = new webpack.DefinePlugin({
+	'process.env': {
+		NODE_ENV: '"debug"'
+	}
+})
 
-module.exports = function(host, port) {
-    config.entry.push('webpack-dev-server/client?http://' + host + ':' + port);
-    return config
+module.exports = function (host, port) {
+	config.entry.push('webpack-dev-server/client?http://' + host + ':' + port)
+	return config
 }
